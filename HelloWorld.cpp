33594de3d0cd7dbdfde83d8a038756ee3e39d9aa
atsune <atsuen01@yahoo.co.jp>
2013-02-24 11:20:26 +0900

    aa

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char dmy;
+
+	printf("Hello World\n");
+	scanf("%c", &dmy);
+	return 0;
+}
+
